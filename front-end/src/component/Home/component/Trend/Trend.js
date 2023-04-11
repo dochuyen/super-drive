b70a6f579a91b86137e59638ee43ba9a44734388
dankieu <kieudinhdan02@gmail.com>
2023-04-11 22:19:47 +0700

    home

--- front-end/src/component/Home/component/Trend/Trend.js
+++ front-end/src/component/Home/component/Trend/Trend.js
@@ -26,28 +26,26 @@ const Trend = () => {
 
   return (
     <div className={cx("wrapper")}>
-      <Row>
-        <div className={cx("title")}>
-          <h5 className={cx("small-title")}>LATEST NEWS</h5>
-          <h2 className={cx("big-title")}>New Trends</h2>
-        </div>
-        <div>
-          {dataGif.map((gif, index) => (
-            <Col key={index}>
-              <div
-                className={cx("gif")}
-                style={{ backgroundImage: `url(${gif.src})` }}
-              ></div>
-              <div className={cx("type")}>
-                <p className={cx("calendar")}>
-                  <AiOutlineCalendar className={cx("icon-calendar")} />
-                  24/03/2023
-                </p>
-                <p className={cx("title-ctt")}>{gif.name}</p>
-              </div>
-            </Col>
-          ))}
-        </div>
+      <div className={cx("title")}>
+        <h5 className={cx("small-title")}>LATEST NEWS</h5>
+        <h2 className={cx("big-title")}>New Trends</h2>
+      </div>
+      <Row sm="12" md="3" lg="3">
+        {dataGif.map((gif, index) => (
+          <Col key={index}>
+            <div
+              className={cx("gif")}
+              style={{ backgroundImage: `url(${gif.src})` }}
+            ></div>
+            <div className={cx("type")}>
+              <p className={cx("calendar")}>
+                <AiOutlineCalendar className={cx("icon-calendar")} />
+                24/03/2023
+              </p>
+              <p className={cx("title-ctt")}>{gif.name}</p>
+            </div>
+          </Col>
+        ))}
       </Row>
     </div>
   );
