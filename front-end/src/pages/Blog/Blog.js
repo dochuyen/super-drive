d919c55881982a767cfd351775c2813b7292e6c3
chuyenpro <115093851+chuyenpro@users.noreply.github.com>
2023-05-11 09:11:00 +0700

    comment

--- front-end/src/pages/Blog/Blog.js
+++ front-end/src/pages/Blog/Blog.js
@@ -1,11 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import { CiUser } from "react-icons/ci";
-import { BiTimeFive } from "react-icons/bi";
-import { FaRegCommentDots } from "react-icons/fa";
-import { AiOutlineRight } from "react-icons/ai";
+
+
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import classNames from "classnames/bind";
@@ -20,8 +17,9 @@ const cx = classNames.bind(styles);
 const Blog = () => {
   const [comments, setComments] = useState([]);
   const [newComment, setNewComment] = useState("");
-
-  const fetchComment = () => {
+  const username = useSelector((state) => state.username);
+const token = JSON.parse(localStorage.getItem("token"));
+  const fetchComment = useCallback(() => {
     axios
       .get(`${process.env.REACT_APP_API_KEY}api/comments/get`)
       .then((res) => {
@@ -29,23 +27,18 @@ const Blog = () => {
         setComments(commentData.data);
       })
       .catch((error) => console.log(error));
-  };
-
+  }, []);
+  
   useEffect(() => {
     fetchComment();
-  }, []);
-
-  const username = useSelector((state) => state.username);
-
-  const token = JSON.parse(localStorage.getItem("token"));
-
+  }, [fetchComment]);
+  
   const postHandle = () => {
     const newFullComment = {
       username: username,
-
       comment: newComment,
     };
-
+  
     fetch(`${process.env.REACT_APP_API_KEY}api/comments`, {
       method: "POST",
       headers: {
@@ -55,30 +48,16 @@ const Blog = () => {
       },
       body: JSON.stringify(newFullComment),
     });
-
+  
     setNewComment("");
   };
+  
 
  
 
   return (
     <>
-      {/* <div className={cx("toolbar")}>
-        <Container>
-          <h3 className={cx("toolbar-title")}>Blog</h3>
-          <div className={cx("directional")} >
-            Home <AiOutlineRight className={cx("icon-tool")} />
-            <span>Blog</span>
-          </div>
-          <div className={cx("blogSearch")}>
-            <form>
-              <input type="text" placeholder="Tìm kiếm blog..."></input>
-              <button className={cx("searchButton")}>Tìm kiếm</button>
-            </form>
-          </div>
-          <BlogHeader></BlogHeader>
-        </Container>
-      </div> */}
+    
       <div>
         <BlogHeader></BlogHeader>
       </div>
@@ -94,102 +73,7 @@ const Blog = () => {
       </div>
       <Container>
       
-        {/* <Row>
-          <Col md={8}>
-            <div className={cx("blog")}>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFwttmJ8hKA9m__MNyYn7WghUocb2Gu9Uoow&usqp=CAU"
-                alt=""
-                width="100%"
-              />{" "}
-              <div className={cx("blog-title")}>
-                <h3>
-                  Dolorum optio tempore voluptas dignissimos cumque fuga qui
-                  quibusdam quia
-                </h3>
-                <div className={cx("blog-post")}>
-                  <span>
-                    <CiUser />
-                    <h6>Đàn Kiều</h6>
-                  </span>
-                  <span>
-                    <BiTimeFive />
-                    <h6>Jan 17, 2020</h6>
-                  </span>
-                  <span>
-                    <FaRegCommentDots />
-                    <h6> 12 Comment</h6>
-                  </span>
-                </div>
-                <p className={cx("blog-text")}>
-                  Similique neque nam consequuntur ad non maxime aliquam quas.
-                  Quibusdam animi praesentium. Aliquam et laboriosam eius aut
-                  nostrum quidem aliquid dicta. Et eveniet enim. Qui velit est
-                  ea dolorem doloremque deleniti aperiam unde soluta. Est cum et
-                  quod quos aut ut et sit sunt. Voluptate porro consequatur
-                  assumenda perferendis dolore.
-                </p>
-                <button>Read More</button>
-              </div>
-            </div>
-          </Col>
-          <Col md={4}>
-            <div className={cx("blog-nav")}>
-              <h3>Recent Posts</h3>
-              <div className={cx("nav-title")}>
-                <img
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFwttmJ8hKA9m__MNyYn7WghUocb2Gu9Uoow&usqp=CAU"
-                  alt=""
-                  width="100px"
-                  height="60px"
-                />
-                <p>
-                  Nihil blanditiis at in nihil autem Similique neque nam
-                  consequuntur ad non maxime aliquam quas. Quibusdam animi
-                  praesentium. Aliquam et laboriosam eius aut nostrum quidem
-                  aliquid dicta. Et eveniet enim. Qui velit est ea dolorem
-                  doloremque deleniti aperiam unde soluta. Est cum et quod quos
-                  aut ut et sit sunt. Voluptate porro consequatur assumenda
-                  perferendis dolore.
-                </p>
-              </div>
-              <div className={cx("nav-title")}>
-                <img
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFwttmJ8hKA9m__MNyYn7WghUocb2Gu9Uoow&usqp=CAU"
-                  alt=""
-                  width="100px"
-                  height="60px"
-                />
-                <p>
-                  Nihil blanditiis at in nihil autem Similique neque nam
-                  consequuntur ad non maxime aliquam quas. Quibusdam animi
-                  praesentium. Aliquam et laboriosam eius aut nostrum quidem
-                  aliquid dicta. Et eveniet enim. Qui velit est ea dolorem
-                  doloremque deleniti aperiam unde soluta. Est cum et quod quos
-                  aut ut et sit sunt. Voluptate porro consequatur assumenda
-                  perferendis dolore.
-                </p>
-              </div>
-              <div className={cx("nav-title")}>
-                <img
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFwttmJ8hKA9m__MNyYn7WghUocb2Gu9Uoow&usqp=CAU"
-                  alt=""
-                  width="100px"
-                  height="60px"
-                />
-                <p>
-                  Nihil blanditiis at in nihil autem Similique neque nam
-                  consequuntur ad non maxime aliquam quas. Quibusdam animi
-                  praesentium. Aliquam et laboriosam eius aut nostrum quidem
-                  aliquid dicta. Et eveniet enim. Qui velit est ea dolorem
-                  doloremque deleniti aperiam unde soluta. Est cum et quod quos
-                  aut ut et sit sunt. Voluptate porro consequatur assumenda
-                  perferendis dolore.
-                </p>
-              </div>
-            </div>
-          </Col>
-        </Row> */}
+     
 
         <br />
         <br />
